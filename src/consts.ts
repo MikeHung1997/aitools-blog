ede256be9d66147f9d8a32b0cd3abbb767f9fa40
MikeHung1997 <mikehung425@gmail.com>
2026-04-21 17:06:55 +0800

    change title

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '欸？愛工具 AI Tool';
+export const SITE_DESCRIPTION = '最新 AI 工具介紹、比較與懶人包。幫你找到最適合的 AI 生產力、寫作、影片和自動化工具。';
